11add5ade7c440f300e973f236af3b3c791d42ac
gsy <1443717855@qq.com>
2021-06-20 13:19:37 +0800

    fix: add

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-class GVue {
+class GVue extends EventTarget {
   constructor($options) {
+    super()
     this.$options = $options;
     this.compile(document.querySelector(this.$options.el))
+    this.init()
+  }
+  init() {
+    for (let key in this.$options.data) {
+      this.observer(this.$options.data, key, this.$options.data[key])
+    }
+  }
+  observer(obj, key, value) {
+    let that = this
+    Object.defineProperty(obj, key, {
+      configurable: true,
+      enumerable: true,
+      get() {
+        console.log('get...')
+        return value
+      },
+      set(newValue) {
+        console.log('set...');
+        let event = new CustomEvent(key, {
+          detail: newValue
+        })
+        that.dispatchEvent(event)
+        value = newValue
+      }
+    })
   }
 
   compile(el) {
@@ -17,9 +43,14 @@ class GVue {
         let textContent = item.textContent;
         let reg = /\{\{\s*(\S+)\s*\}\}/g;
         if (reg.test(textContent)) {
-          console.log(this.$options.data[RegExp.$1])
           item.textContent = textContent.replace(reg, this.$options.data[RegExp.$1])
         }
+        this.addEventListener(RegExp.$1, e => {
+          console.log('触发了', e);
+          let oldValue = this.$options.data[RegExp.$1];
+          let reg = new RegExp(oldValue);
+          item.textContent = item.textContent.replace(reg, e.detail);
+        })
       }
     })
   }
